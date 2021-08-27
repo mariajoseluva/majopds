be0b0d789496257a42a35e45356214e08e9c3ac1
mariajoseluva <50151788+mariajoseluva@users.noreply.github.com>
2021-08-26 18:52:44 -0700

    Update index.php

--- index.php
+++ index.php
@@ -6,10 +6,10 @@
 </head>
 <body>
     <h1>
-        Servidor de María José // 000webhost
+        Servidor de María José // git
     </h1>
     <h2>Una foto de algunos de mis amigos y yo</h2>
-    <img src="amigos.jpg" alt="amigos1" height="20%" width="20%" />
+    <img src="amigos1.jpeg" alt="amigos1" height="20%" width="20%" />
 
 
     <h3>Info personal:</h3>
@@ -23,6 +23,7 @@
 
     <h2>Jonas Brothers</h2>
     <img src="https://www.androidcentral.com/sites/androidcentral.com/files/styles/large/public/article_images/2020/04/jonas-brothers-happiness-continues-hero.jpg" alt="bts" width="25%" height="25%">
+    <a href="https://mariajoseluva.000webhostapp.com/">Visita mi página 000webhost</a>
     <a href="https://maria-jose.mariajoseluva.repl.co/">Visita mi página repl.it</a>
 
 </body>
